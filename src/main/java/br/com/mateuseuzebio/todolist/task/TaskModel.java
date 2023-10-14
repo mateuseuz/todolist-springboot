e0df56bc79cc7876618f1aa6bf709a914a89c2bb
Mateus Euzébio <mateuseuzebio@hotmail.com>
2023-10-14 18:12:10 -0300

    Dockerfile deploy

---
+++ src/main/java/br/com/mateuseuzebio/todolist/task/TaskModel.java
@@ -0,0 +1,38 @@
+package br.com.mateuseuzebio.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(length = 30)
+    private String title;
+    private String description;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("O campo title não pode conter mais que 30 caracteres!");
+        }
+        this.title = title;
+    }
+}
